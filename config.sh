91d6ec1bea5a042ba00124f53f17b3f922b853bf
jfarcher <jf_archer@yahoo.com>
2015-08-25 15:28:06 +0100

    A number of changes in this release, all questions have been moved to the beginning this is more sane and allows the rest of the process to be left. Also included is a check to see which version of Pi the script is running on, requests to see if a proxy server is required, and whether puppet is used to retrieve config.

--- config.sh
+++ config.sh
@@ -1,22 +1,82 @@
 #!/bin/bash
-
 echo "Starting Pi wallboard setup"
 echo ""
-echo "please make sure you are running as root (sudo -i)"
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+else
+
+echo "Starting Pi wallboard setup"
 echo ""
 echo "Please input the hostname you wish to use for this wallboard"
 read hname
 echo "Using: $hname"
+echo "Please enter the local domain suffix eg. example.com"
+read dname
+echo "FQDN will be $hname.$dname"
 
 echo "$hname" > /etc/hostname
-hostname $hname
+sed '/^127.0.0.1/ d' -i /etc/hosts
+echo "127.0.0.1 $hname.$dname $hname localhost" >> /etc/hosts
+/etc/init.d/hostname.sh
+echo ""
+
+echo "Lets reset the password for the pi user"
+echo "Please make sure you store this in the password safe"
+passwd pi
+echo ""
+
+echo "Please enter the URL to be displayed"
+read url
+if [ ! -z $url ]; then
+url = http://status.aws.amazon.com/
+fi
+echo "Using: $url"
+cat <<EOF>/etc/wallboardurl.conf
+$url
+EOF
+
+while true; do
+    read -p "Will you be using puppet to control your display board? (Y/N)" yn
+    case $yn in
+        [Yy]* ) puppet=1; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+while true; do
+    read -p "Does your Pi require the use of a proxy to gain access to the internet? (Y/N)" yn
+    case $yn in
+	[Yy]* ) echo "Please enter the proxy server (http://server:port)"; read proxy;break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+if [ ! -z $proxy ]; then
+cat <<EOF> /etc/apt/apt.conf.d/00proxy
+Acquire::http::Proxy "$proxy";
+EOF
+export http{,s}_proxy="$proxy"
+fi
+
+
+
+
+echo ""
+echo "Thanks, The digital display will now continue installing you probably have enough time to make a brew!"
+echo "Once the install completes it will automatically reboot and you should see the desired display"
+echo "If you are using puppet to control the URL displayed, you can now place a config file on your puppetmaster."
+echo ""
+
 
 apt-get update
 apt-get upgrade -y
+apt-get install chromium vim x11-xserver-utils xwit sqlite3 libnss3 unclutter ttf-mscorefonts-installer openjdk-7-jdk icedtea-7-plugin -y
+ln -s /usr/lib/jvm/java-7-openjdk-amd64/jre/lib/amd64/IcedTeaPlugin.so /usr/lib/chromium/plugins/IcedTeaPlugin.so
 
-apt-get install chromium vim x11-xserver-utils ttf-mscorefonts-installer xwit sqlite3 libnss3 unclutter puppet -y
 
-sed -i s/START=no/START=yes/g /etc/default/puppet
 update-rc.d lightdm enable 2
 sed /etc/lightdm/lightdm.conf -i -e "s/^#autologin-user=.*/autologin-user=pi/"
 
@@ -38,15 +98,35 @@ cat <<EOF>/usr/local/sbin/tvoff.sh
 tvservice -o
 EOF
 chmod +x /usr/local/sbin/tvoff.sh
-echo "disable_overscan=0" >>/boot/config.txt
 
-cat <<EOF>/etc/wallboardurl.conf
-http://status.aws.amazon.com/
-EOF
+rev="`uname -m`"
+if [ "$rev" == "armv6l" ]; then
+echo "Pi 1"
+echo "disable_overscan=0" >>/boot/config.txt
+fi
+
+if [ "$rev" == "armv7l" ]; then
+echo "Pi 2"
+echo "disable_overscan=1" >>/boot/config.txt
+fi
+
+
+
+
 
 cat <<EOF>>/var/spool/cron/crontabs/root
 30 17 * * * /usr/local/sbin/tvoff.sh
 00 08 * * * /usr/local/sbin/tvon.sh
 EOF
 chmod 600 /var/spool/cron/crontabs/root
+
+###puppet
+if [ $puppet == "1" ]; then
+apt-get install puppet -y
+sed -i s/START=no/START=yes/g /etc/default/puppet
+puppet agent -t
+puppet agent -t
+fi
+
 /sbin/reboot
+fi
